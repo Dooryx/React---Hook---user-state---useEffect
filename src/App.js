12591cf617f857e67da8a87566cb63d0f6c98c33
Juan Ponciano <juanignacioponciano04@gmail.com>
2023-05-10 14:31:46 -0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";        
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Divider } from 'primereact/divider';
 
 function App() {
+  const [texto, setTexto] = useState('');
+
+  
+
+  function cambioTexto(e) {
+    const inputValue = e.target.value;
+    if (inputValue.length >= 24) {
+      setTexto(inputValue);
+    } else {
+      setTexto(inputValue.slice(0, 24));
+    }
+  }
+  function contarPalabras(texto) {
+    const palabras = texto.trim().split(' ');
+    return palabras.length;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Divider>
+      <p>
+        <InputTextarea  placeholder="Solo hasta 25 caracteres maximo" value={texto} onChange={cambioTexto} cols="100" rows="5" maxLength={25} ></InputTextarea>
+      </p>
+      <Divider>
+      {texto.length > 24 && ( <p style={{ color: 'red' }}>Ha alcanzado el límite de caracteres</p>)}
+      <p>Cantidad de caracteres ingresados: {texto.length}</p>
+      </Divider>
+      <Divider>
+      <p>Cantidad de palabras ingresadas: {contarPalabras(texto)}</p>
+      </Divider>
+    </Divider>
+    
   );
 }
 
